0ed86322ad291b074fca07f1ac6f172506f2d5c1
giahungbk <giahunghust@gmail.com>
2020-01-07 18:11:34 +0700

    destroy shuffle, fix query div image

--- src/helper.js
+++ src/helper.js
@@ -1,27 +1,53 @@
 /**
  * Fake $ element
- * @param {Element} scope tên element muốn query
- * @param {String} selector tên class, id, tagName
- * @param {String} type chọn Node hoặc là đơn
+ * @param {Element} scope Tên element muốn query
+ * @param {String} selector Tên class, id, tagName
+ * @param {String} typeQuery Chọn kiểu muốn query Node hoặc NodeList
+ * @return {NodeList} type query là NodeList 
+ * @return {Node} type query là Node 
  */
-const $ = (scope, selector, typeQuery = "") => {
-    if (typeQuery === "ALL") {
+const $ = (scope, selector, typeQuery = "Node") => {
+    if (typeQuery === "NodeList") {
         return scope.querySelectorAll(selector);
     }
     return scope.querySelector(selector);
 };
 
 /**
- * Them su kien cho element
- * @param {*} target the muon them su kien
- * @param {*} type kieu su kien
- * @param {*} callback callback funtion
+ * Thêm sự kiện cho một element
+ * @param {Element} target Element muốn thêm sự kiện
+ * @param {String} type Loại sự kiện
+ * @param {Function} callback callback funtion
  */
 const $on = (target, type, callback) => {
     target.addEventListener(type, callback);
 };
 
-export {
-    $,
-    $on,
-}
+/**
+ * Xoá sự kiện của một element
+ * @param {Element} target Element muốn xoá sự kiện
+ * @param {String} type Loại sự kiện
+ * @param {Function} callback callback funtion
+ */
+const $off = (target, type, callback) => {
+  target.removeEventListener(type, callback);
+};
+
+/**
+   * Hiển thị image
+   * @param {Element} activeElement Image đang được hiển thị
+   * @param {Element} imageFirst Thẻ image đầu tiên trong 1 Nodelist
+   */
+const displayImage = (activeElement, imageFirst) => {
+  if (activeElement.nextElementSibling) {
+    activeElement.nextElementSibling.style.display = "block";
+    activeElement.nextElementSibling.setAttribute("data-active", "active");
+  } else {
+    imageFirst.style.display = "block";
+    imageFirst.setAttribute("data-active", "active");
+  }
+  activeElement.style.display = "none";
+  activeElement.removeAttribute("data-active");
+};
+
+export { $, $on, $off, displayImage };
--- src/index.js
+++ src/index.js
@@ -1,119 +1,199 @@
-import { $, $on } from './helper';
+import { $, $on, $off, displayImage } from "./helper";
 
 export default class ShuffleImages {
-    constructor(options) {
-      this.options = options;
-      this.init();
-    }
-  
-    init() {
-      let shuffleImageElements = $(document, this.options.target, "ALL");
-      shuffleImageElements.forEach(el => {
-        this.shuffleHandler(el);
-      });
-    }
-
-    destroy() {
-        
-    }
-
-    shuffleHandler(elementNode) {
-      const imgAllElement = $(elementNode, "img", "ALL");
-      let distance = 0;
-      let triggerTime = null;
-  
-      const defaults = {
-        type: "imageMouseMove",
-        mouseMoveTrigger: 50,
-        hoverTrigger: 100,
-        scrollTrigger: 100
-      };
-      const settings = {
-        ...defaults,
-        ...this.options
-      };
-  
-      imgAllElement.forEach((imgEl, i) => {
-        if (i !== 0) {
-          imgEl.style.display = "none";
-        } else {
-          imgEl.setAttribute("data-active", "active");
-        }
-      });
-  
-      switch (settings.type) {
-        case "imageMouseMove":
-          $on(elementNode, "mousemove", event => {
-            let active = $(elementNode, "img[data-active]");
-            let math = Math.round(
-              Math.sqrt(Math.pow(event.clientY, 2) + Math.pow(event.clientX, 2))
-            );
-  
-            if (Math.abs(math - distance) > settings.mouseMoveTrigger) {
-              this.displayImg(active, imgAllElement[0]);
-              distance = math;
-            }
-          });
-          break;
-        case "imageHover":
-          $on(elementNode, "mouseover", () => {
-            triggerTime = setInterval(() => {
-              let active = $(elementNode, "img[data-active]");
-              this.displayImg(active, imgAllElement[0]);
-            }, settings.hoverTrigger);
-          });
-          $on(elementNode, "mouseout", () => {
-            clearInterval(triggerTime);
-          });
-          break;
-  
-        case "documentMouseMove":
-          $on(document, "mousemove", event => {
-            let active = $(elementNode, "img[data-active]");
-            let math = Math.round(
-              Math.sqrt(Math.pow(event.clientY, 2) + Math.pow(event.clientX, 2))
-            );
-  
-            if (Math.abs(math - distance) > settings.mouseMoveTrigger) {
-              this.displayImg(active, imgAllElement[0]);
-              distance = math;
-            }
-          });
-          break;
-  
-        case "documentScroll":
-          $on(document, "scroll", () => {
-            let math = window.pageYOffset;
-            let active = $(elementNode, "img[data-active]");
-            if (Math.abs(math - distance) > settings.scrollTrigger) {
-              this.displayImg(active, imgAllElement[0]);
-              distance = math;
-            }
-          });
-          break;
-        default:
-          break;
-      }
-    }
-  
+  /**
+   *
+   * @param {Object} options
+   */
+  constructor(options) {
+    this.options = options;
     /**
-     * Hien thi image
-     * @param {Element} activeElement image dang duoc active
-     * @param {Element} imageFirst the image dau tien
+     * @param {NodeList} shuffleImageElements Get NodeList 
      */
-    displayImg(activeElement, imageFirst) {
-      if (activeElement.nextElementSibling) {
-        activeElement.style.display = "none";
-        activeElement.nextElementSibling.style.display = "block";
-        activeElement.nextElementSibling.setAttribute("data-active", "active");
-        activeElement.removeAttribute("data-active");
-      } else {
-        activeElement.style.display = "none";
-        imageFirst.style.display = "block";
-        imageFirst.setAttribute("data-active", "active");
-        activeElement.removeAttribute("data-active");
-      }
-    }
-    
+    this.shuffleImageElements = $(document, this.options.target, "NodeList");
+    /**
+     * @param {Boolean} isInit xét điều khiện init hoặc là destroy
+     */
+    this.isInit = true;
+    /**
+     * @param {number} distance Biến điều kiện để so sánh khi di chuyển chuột
+     */
+    this.distance = null;
+    /**
+     * @param {number} triggerTime Biến lưu trữ setInterval 
+     */
+    this.triggerTime = null;
+    /**
+     * @param {Node} triggerTime Node Element
+     */
+    this.node = null;
+
+    /**
+     * @param {Object} defaults options mặc định
+     */
+    this.defaults = {
+      type: "imageMouseMove",
+      mouseMoveTrigger: 200,
+      hoverTrigger: 2000,
+      scrollTrigger: 100
+    };
+
+    /**
+     * @param {Object} settings options đầu vào
+     */
+    this.settings = {
+      ...this.defaults,
+      ...this.options
+    };
+
+    //* thay đổi con trỏ this của hàm
+    this.imageMouseMoveHandler = this.imageMouseMoveHandler.bind(this);
+    this.imageMouseOverHandler = this.imageMouseOverHandler.bind(this);
+    this.imageMouseOverHandler = this.imageMouseOverHandler.bind(this);
+    this.imageMouseOutHandler = this.imageMouseOutHandler.bind(this);
+    this.documentScrollHandler = this.documentScrollHandler.bind(this);
   }
-  
+
+  /**
+   * Hàm khởi tạo shuffle
+   */
+  init() {
+    if (this.isInit) {
+      this.isInit = false;
+      if (this.shuffleImageElements.length > 0) {
+        this.shuffleImageElements.forEach(el => {
+          this.shuffleHandler(el);
+        });
+      }
+      return;
+    }
+  }
+
+  /**
+   * Hàm huỷ shuffle
+   */
+  destroy() {
+    if (!this.isInit) {
+      this.isInit = true;
+      if (this.shuffleImageElements.length > 0) {
+        this.shuffleImageElements.forEach(el => {
+          this.destroyShuffleHandler(el);
+        });
+      }
+      return;
+    }
+  }
+
+  /**
+   * Hàm xử lý shuffle
+   * @param {Node} elementNode 
+   */
+  shuffleHandler(elementNode) {
+    const imgAllElement = $(elementNode,elementNode.firstElementChild.localName,"NodeList");
+    this.node = elementNode;
+
+    imgAllElement.forEach((imgEl, i) => {
+      if (i !== 0) {
+        imgEl.style.display = "none";
+      } else {
+        imgEl.setAttribute("data-active", "active");
+      }
+    });
+
+    switch (this.settings.type) {
+      case "imageMouseMove":
+        $on(elementNode, "mousemove", this.imageMouseMoveHandler);
+        break;
+      case "imageHover":
+        $on(elementNode, "mouseover", this.imageMouseOverHandler);
+        $on(elementNode, "mouseout", this.imageMouseOutHandler);
+        break;
+      case "documentMouseMove":
+        $on(document, "mousemove", this.imageMouseMoveHandler);
+        break;
+      case "documentScroll":
+        $on(document, "scroll", this.documentScrollHandler);
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Hàm huỷ shuffle và các sự kiện khác
+   * @param {Node} elementNode 
+   */
+  destroyShuffleHandler(elementNode) {
+    const imgAllElement = $(elementNode,elementNode.firstElementChild.localName,"NodeList");
+
+    imgAllElement.forEach((imgEl, i) => {
+      if (imgEl.hasAttribute("data-active") || imgEl.hasAttribute("style")) {
+        imgEl.removeAttribute("data-active");
+        imgEl.removeAttribute("style");
+      }
+    });
+
+    switch (this.settings.type) {
+      case "imageMouseMove":
+        $off(elementNode, "mousemove", this.imageMouseMoveHandler);
+        break;
+      case "imageHover":
+        $off(elementNode, "mouseover", this.imageMouseOverHandler);
+        $off(elementNode, "mouseout", this.imageMouseOutHandler);
+        break;
+      case "documentMouseMove":
+        $off(document, "mousemove", this.imageMouseMoveHandler);
+        break;
+      case "documentScroll":
+        $off(document, "scroll", this.documentScrollHandler);
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Hàm xử lý hiển thị image khi di chuyển chuột vào element or document có điều kiện khoảng cách 
+   */
+  imageMouseMoveHandler() {
+    let active = $(this.node, "[data-active]");
+    let math = Math.round(
+      Math.sqrt(Math.pow(event.clientY, 2) + Math.pow(event.clientX, 2))
+    );
+
+    if (Math.abs(math - this.distance) > this.settings.mouseMoveTrigger) {
+      displayImage(active, this.node.firstElementChild);
+      this.distance = math;
+    }
+  }
+
+  /**
+   * Hàm xử lý hiển thị image khi di chuyển chuột vào element có điều kiện thời gian
+   */
+  imageMouseOverHandler() {
+    this.triggerTime = setInterval(() => {
+      let active = $(this.node, "[data-active]");
+      displayImage(active, this.node.firstElementChild);
+    }, this.settings.hoverTrigger);
+  }
+
+  /**
+   * Hàm xử lý hiển thị image khi di chuyển ra ngoài element
+   */
+  imageMouseOutHandler() {
+    clearInterval(this.triggerTime);
+  }
+
+  /**
+   * Hàm xử lý hiển thị image khi scroll document
+   */
+  documentScrollHandler() {
+    let math = window.pageYOffset;
+    let active = $(this.node, "[data-active]");
+
+    if (Math.abs(math - this.distance) > this.settings.scrollTrigger) {
+      displayImage(active, this.node.firstElementChild);
+      this.distance = math;
+    }
+  }
+}
